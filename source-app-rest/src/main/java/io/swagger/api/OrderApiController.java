5af5c0995e4a996cdd2cd7f34c7db7d91e7a0228
Nanda.GS <nandagopan.gs@cognizant.com>
2018-03-14 22:03:57 +0530

    Fixed 404 issue caused by context path / base url specified in application.yml

--- source-app-rest/src/main/java/io/swagger/api/OrderApiController.java
+++ source-app-rest/src/main/java/io/swagger/api/OrderApiController.java
@@ -1,31 +1,26 @@
 package io.swagger.api;
 
-import io.swagger.model.CreateOrderRequest;
-import io.swagger.model.CreateOrderResponse;
-import io.swagger.model.OrderDetails;
-import io.swagger.service.OrderService;
-import io.swagger.annotations.*;
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-import javax.validation.constraints.*;
-import javax.validation.Valid;
+import io.swagger.annotations.ApiParam;
+import io.swagger.model.CreateOrderRequest;
+import io.swagger.model.CreateOrderResponse;
+import io.swagger.model.OrderDetails;
+import io.swagger.service.OrderService;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-03-05T12:35:53.697Z")
 
-@Controller
+@RestController
+@RequestMapping("/api")
 public class OrderApiController implements OrderApi {
 
 	Logger logger = LoggerFactory.getLogger(getClass());
